919b4d3f1e523cc6ad09c17a6e43d14d4eea66d0
eConnah <63052937+eConnah@users.noreply.github.com>
2023-10-04 14:20:47 +0100

    TicTacToe comments

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,157 +1,220 @@
-﻿class Program {
-  public static char[,] grid = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+﻿internal class Program
+{
+  // Create a 3x3 grid for the game
+  public static char[,] grid = {
+    { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' }
+  };
+
+  // Keep track of the number of filled spaces on the grid
   public static int filledSpaces = 0;
-  public static void Main (string[] args) {
+
+  public static void Main(string[] args)
+  {
     bool gameOver = false;
     int x = -1;
     int y = -1;
-    Console.WriteLine("WELCOME TO NOUGHTS AND CROSSES\n\n");
-    displayGrid();
 
-    while(!gameOver & filledSpaces < 9){
+    // Display a welcome message
+    Console.WriteLine("WELCOME TO NOUGHTS AND CROSSES\n\n");
+
+    // Display the initial grid
+    DisplayGrid();
+
+    // Continue the game until the game is over or all spaces are filled
+    while (!gameOver & filledSpaces < 9)
+    {
       x = -1;
       y = -1;
-      do{
+
+      // Player One's turn (O)
+      do
+      {
         Console.WriteLine("\nPlayer One (O), make your move - enter your coordinates using x and y\n Enter your x value:");
         x = Convert.ToInt32(Console.ReadLine()) - 1;
-        
+
         Console.WriteLine("\nNow enter your y value:");
         y = Convert.ToInt32(Console.ReadLine()) - 1;
-        
-      }while(!checkMove(x,y));
-      
-      oMove(x,y);
-      displayGrid();
+
+      } while (!CheckMove(x, y));
+
+      // Make O's move on the grid and display the updated grid
+      OMove(x, y);
+      DisplayGrid();
 
       x = -1;
       y = -1;
-      
-      do{
-        if(checkGameWon()=='o' | (filledSpaces == 9)){
+
+      // Player Two's turn (X)
+      do
+      {
+        // Check if Player One has already won the game or all spaces are filled
+        if (CheckGameWon() == 'o' | (filledSpaces == 9))
+        {
           break;
-          }
-          
+        }
+
         Console.WriteLine("\nPlayer Two (X), make your move - enter your coordinates using x and y\n Enter your x value:");
         x = Convert.ToInt32(Console.ReadLine()) - 1;
-        
+
         Console.WriteLine("\nNow enter your y value:");
         y = Convert.ToInt32(Console.ReadLine()) - 1;
-        
-      }while(!checkMove(x,y));
 
-      if(checkGameWon() != 'o' & filledSpaces < 9 ){
-        xMove(x,y);
-        displayGrid();
+      } while (!CheckMove(x, y));
+
+      // If Player One hasn't won and there are still empty spaces, make X's move and display the updated grid
+      if (CheckGameWon() != 'o' & filledSpaces < 9)
+      {
+        XMove(x, y);
+        DisplayGrid();
       }
-      
 
-      if(checkGameWon()=='o'){
+      // Check if Player One has won, Player Two has won, or it's a draw
+      if (CheckGameWon() == 'o')
+      {
         gameOver = true;
         Console.WriteLine("O has won the game!");
-      }else if(checkGameWon() =='x'){
+      }
+      else if (CheckGameWon() == 'x')
+      {
         gameOver = true;
         Console.WriteLine("X has won the game!");
-      }else if(filledSpaces == 9){
+      }
+      else if (filledSpaces == 9)
+      {
         gameOver = true;
         Console.WriteLine("No winner this time, better luck next time");
       }
     }
-    
   }
-  public static void displayGrid(){
-    for(int i = 0; i < 3; i++){
+
+  // Display the current state of the grid
+  public static void DisplayGrid()
+  {
+    for (int i = 0; i < 3; i++)
+    {
       Console.WriteLine("-----------");
-      for(int j = 0; j < 3; j++){
-        Console.Write($"|{grid[i,j]}| ");
+      for (int j = 0; j < 3; j++)
+      {
+        Console.Write($"|{grid[i, j]}| ");
       }
       Console.WriteLine("\n-----------");
     }
   }
-  
-  public static void oMove(int x, int y){
-      grid[x,y] = 'O';
-      filledSpaces++;
-    
-    }
 
-    public static void xMove(int x, int y){
-      grid[x,y] = 'X';
-      filledSpaces++;
-    
-    }
-
-  
-  
-  public static bool checkMove(int x, int y)
+  // Make O's move on the grid
+  public static void OMove(int x, int y)
   {
-    bool valid = false;
-    if(x < 3 & x >= 0){
-      if(y < 3 & y>= 0){
-        if(grid[x,y] == '-'){
-          valid = true;
-          }else{
-          Console.WriteLine("Space taken, please try again");
-          }
-      }else{
-        Console.WriteLine("Invalid input, please enter values between 1 and 3");
-      }
-    }else{
-        Console.WriteLine("Invalid input, please enter values between 1 and 3");
-      }
-     return valid;
+    grid[x, y] = 'O';
+    filledSpaces++;
   }
 
-  
-  public static char checkGameWon()
+  // Make X's move on the grid
+  public static void XMove(int x, int y)
+  {
+    grid[x, y] = 'X';
+    filledSpaces++;
+  }
+
+  // Check if a move is valid
+
+  public static bool CheckMove(int x, int y)
+  {
+    bool valid = false;
+
+    // Check if the coordinates are within the grid
+    if (x < 3 & x >= 0)
+    {
+      if (y < 3 & y >= 0)
+      {
+        // Check if the space is available ('-')
+        if (grid[x, y] == '-')
+        {
+          valid = true;
+        }
+        else
+        {
+          Console.WriteLine("Space taken, please try again");
+        }
+      }
+      else
+      {
+        Console.WriteLine("Invalid input, please enter values between 1 and 3");
+      }
+    }
+    else
+    {
+      Console.WriteLine("Invalid input, please enter values between 1 and 3");
+    }
+    return valid;
+  }
+
+  // Check if any player has won the game
+  public static char CheckGameWon()
   {
     int ohoriz = 0;
     int overt = 0;
     int xhoriz = 0;
     int xvert = 0;
 
-    
-   for(int i = 0; i < 3; i++){
-     
-      for(int j = 0; j < 3; j++){
-        if(grid[i,j]=='O'){
-          ohoriz++;          
-        }else if(grid[i,j] == 'X'){
+    // Check for winning conditions horizontally and vertically
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        if (grid[i, j] == 'O')
+        {
+          ohoriz++;
+        }
+        else if (grid[i, j] == 'X')
+        {
           xhoriz++;
         }
-        
-        
-        
-        if(grid[j,i] == 'O'){
+
+        if (grid[j, i] == 'O')
+        {
           overt++;
-        }else if(grid[j,i] == 'X'){
+        }
+        else if (grid[j, i] == 'X')
+        {
           xvert++;
-        }    
+        }
       }
-        if(ohoriz < 3){
-          ohoriz = 0;
-        }
-        if(xhoriz < 3){
-          xhoriz = 0;
-        }
-        if(overt < 3){
-          overt = 0;
-        }
-        if(xvert < 3){
-          xvert = 0;
-        
-        }
-     
-     
+
+      // Reset the counters if not a winning condition
+      if (ohoriz < 3)
+      {
+        ohoriz = 0;
+      }
+      if (xhoriz < 3)
+      {
+        xhoriz = 0;
+      }
+      if (overt < 3)
+      {
+        overt = 0;
+      }
+      if (xvert < 3)
+      {
+        xvert = 0;
+      }
     }
-    if((ohoriz + overt) > (xhoriz + xvert)){
+
+    // Determine the winner based on the counters
+    if ((ohoriz + overt) > (xhoriz + xvert))
+    {
       return 'o';
-    }else if(xhoriz + xvert > ohoriz + overt){
+    }
+    else if (xhoriz + xvert > ohoriz + overt)
+    {
       return 'x';
-    }else if(xhoriz + xvert == 0){
+    }
+    else if (xhoriz + xvert == 0)
+    {
       return 'n';
-    }else{
+    }
+    else
+    {
       return 'd';
     }
   }
 }
-

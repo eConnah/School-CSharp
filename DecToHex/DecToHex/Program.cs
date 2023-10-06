dd2d7c92909bfba132a32dcdb5e33e344ce7dc1c
eConnah <63052937+eConnah@users.noreply.github.com>
2023-10-06 14:13:59 +0100

    Completed DecToHex

--- DecToHex/DecToHex/Program.cs
+++ DecToHex/DecToHex/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int userInput;
+        string hexNumber;
+
+        //Set variables
+        Console.Write("Please input the number you would like to convert to hex: ");
+        userInput = Convert.ToInt32(Console.ReadLine());
+
+        //Convert
+        hexNumber = Convert.ToString(userInput, 16).ToUpper();
+
+        //Output
+        Console.WriteLine($"The hex number is: {hexNumber}");
+    }
+}

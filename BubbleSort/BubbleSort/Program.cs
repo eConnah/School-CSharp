c7cecaf1518f9420115427e7477fb013f5745ea6
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-10-06 12:17:32 +0100

    Created BubbleSort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,67 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int[] numbers;
+        int[] sortedNumbers;
+
+        //Set variables
+        numbers = new int[] { 19, 5, 0, -1, 10, -5 };
+
+        //Output
+        Console.WriteLine("Here is the array before sorting: ");
+        foreach (int number in numbers)
+        {
+            Console.Write($"{number}, ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //Sort
+        sortedNumbers = Sorted(numbers);
+
+        //Output
+        foreach (int sortedNumber in sortedNumbers)
+        {
+            Console.Write($"{sortedNumber}, ");
+        }
+        Console.WriteLine();
+    }
+
+    private static int[] Sorted(int[] numbers)
+    {
+        //Declare variables
+        int tempNumOne;
+        int tempNumTwo;
+        int sorted;
+
+        //Set variable
+        sorted = -1;
+
+        //Loop
+        while (sorted != 0)
+        {
+            sorted = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (i < numbers.Length - 1)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        tempNumOne = numbers[i];
+                        tempNumTwo = numbers[i + 1];
+                        numbers[i] = tempNumTwo;
+                        numbers[i + 1] = tempNumOne;
+                        sorted++;
+                    }
+                }
+            }
+        }
+
+
+        //Output
+        return numbers;
+    }
+}

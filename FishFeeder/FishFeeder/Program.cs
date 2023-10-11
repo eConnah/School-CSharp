bf710b379b0aeacbb5628c4c3108c7ddb1d02e6b
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-10-11 14:04:24 +0100

    Created FishFeeder

---
+++ FishFeeder/FishFeeder/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int fishSize;
+        string fishState;
+
+        //Set variables
+        fishSize = 1;
+        fishState = "Fish";
+        Console.WriteLine($"{fishState} is of size {fishSize}");
+
+        //Loop
+        do
+        {
+            (fishState, fishSize) = Feed(fishSize);
+        } while (!String.Equals(fishState, "FISH"));
+
+        Console.WriteLine($"It is now a big {fishState}.");
+    }
+
+    private static (string, int) Feed(int fishSize)
+    {
+        fishSize++;
+        Console.WriteLine("Fish fed.");
+        if (fishSize >= 5)
+        {
+            return ("FISH", fishSize);
+        }
+        else
+        {
+            return ("Fish", fishSize);
+        }
+    }
+}

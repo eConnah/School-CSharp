e9a7b329afdfd85e7481bc3889aa4bc11945e4e6
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-10-11 14:52:07 +0100

    Changed FishFeeder to use Object Orientated Programming

--- FishFeeder/FishFeeder/Program.cs
+++ FishFeeder/FishFeeder/Program.cs
@@ -1,36 +1,39 @@
-﻿internal class Program
+﻿Fish cat = new Fish("Fish", 1);
+Fish dog = new Fish("Fish", 1);
+
+Console.WriteLine(cat.GetState());
+while (dog.GetState() != "FISH")
 {
-    private static void Main(string[] args)
+    dog.Feed();
+}
+Console.WriteLine($"The dog is now a big {dog.GetState()}");
+
+internal class Fish
+{
+    private string state;
+    private int size;
+    public Fish(string fishState, int fishSize)
     {
-        //Declare variables
-        int fishSize;
-        string fishState;
-
-        //Set variables
-        fishSize = 1;
-        fishState = "Fish";
-        Console.WriteLine($"{fishState} is of size {fishSize}");
-
-        //Loop
-        do
-        {
-            (fishState, fishSize) = Feed(fishSize);
-        } while (!String.Equals(fishState, "FISH"));
-
-        Console.WriteLine($"It is now a big {fishState}.");
+        state = fishState;
+        size = fishSize;
     }
 
-    private static (string, int) Feed(int fishSize)
+    public void Feed()
     {
-        fishSize++;
-        Console.WriteLine("Fish fed.");
-        if (fishSize >= 5)
+        size++;
+        if (size >= 5)
         {
-            return ("FISH", fishSize);
+            state = "FISH";
         }
-        else
-        {
-            return ("Fish", fishSize);
-        }
+    }
+
+    public string GetState()
+    {
+        return state;
+    }
+
+    public int GetSize()
+    {
+        return size;
     }
 }

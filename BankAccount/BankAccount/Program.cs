65f63c07cb1e4cb607bfde5cd0403b444493a811
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-10-13 09:55:00 +0100

    Created BankAccount

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,24 @@
+﻿class Banking
+{
+    private int Balance;
+
+    public Banking(int startingAmount)
+    {
+        Balance = startingAmount;
+    }
+
+    public void IncreaseBalance(int amount)
+    {
+        Balance += amount;
+    }
+
+    public void DecreaseBalance(int amount)
+    {
+        Balance -= amount;
+    }
+
+    public int GetBalance()
+    {
+        return Balance;
+    }
+}

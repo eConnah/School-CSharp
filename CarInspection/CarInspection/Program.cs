65fc6229b4886e571bd4991a4d25ae1c1a4b2037
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-10-13 09:48:19 +0100

    Created CarInspection

---
+++ CarInspection/CarInspection/Program.cs
@@ -0,0 +1,55 @@
+﻿Car one = new("GY12 RZB", "Lambourghini");
+one.SetMileage();
+one.SetInspectionDate();
+Console.WriteLine($"Number Plate: {one.GetNumberPlate()}");
+Console.WriteLine($"Car Make: {one.GetCarMake()}");
+Console.WriteLine($"Mileage: {one.GetMileage()}");
+Console.WriteLine($"Inspection Date: {one.GetInspectionDate()}");
+
+class Car
+{
+    private string numberPlate;
+    private string carMake;
+    private int mileage;
+    private string inspectionDate = string.Empty;
+
+    public Car(string plate, string make)
+    {
+        carMake = make;
+        numberPlate = plate;
+    }
+
+    public string GetNumberPlate()
+    {
+        return numberPlate;
+    }
+
+    public string GetCarMake()
+    {
+        return carMake;
+    }
+
+    public int GetMileage()
+    {
+        return mileage;
+    }
+
+    public string GetInspectionDate()
+    {
+        return inspectionDate;
+    }
+
+    public void SetInspectionDate()
+    {
+        //Declare variable
+        DateTime currentDate = DateTime.Now;
+
+        //Return
+        inspectionDate = currentDate.ToShortDateString();
+    }
+
+    public void SetMileage()
+    {
+        mileage = 0;
+    }
+}

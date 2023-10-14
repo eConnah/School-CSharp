dfbd065523296ee1a4a2d5963822e60fcf517ae7
eConnah <63052937+eConnah@users.noreply.github.com>
2023-10-14 22:28:36 +0100

    Added BankAccount Menu

--- BankAccount/BankAccount/Program.cs
+++ BankAccount/BankAccount/Program.cs
@@ -1,42 +1,144 @@
 ﻿using System.Net.NetworkInformation;
-//Declare variables
-string choiceString;
-int choiceInt;
-do
+class Program
 {
-    do
+    private static int SwitchPin;
+    private static void Main(string[] args)
     {
-        //Menu
-        Console.WriteLine("Choose an option:");
-        Console.WriteLine("1. Create a new account");
-        Console.WriteLine("2. Deposit money");
-        Console.WriteLine("3. Withdraw money");
-        Console.WriteLine("4. Check balance");
-        Console.WriteLine("5. Exit");
+        //Declare variables
+        string choiceString;
+        int choiceInt;
+        Banking userAccount;
 
         //Set variables
-        choiceString = Console.ReadLine()?.Trim() ?? string.Empty;
-    } while (int.TryParse(choiceString, out choiceInt) == false || choiceInt < 1 || choiceInt > 5);
+        userAccount = CreateAccount();
 
-    //Switch
-    switch (choice)
-    {
-        case "1":
-        case "2":
-        case "3":
-        case "4":
-        case "5":
-        default:
-            break;
+        //Loop
+        do
+        {
+            do
+            {
+                //Menu
+                Console.Clear();
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Create a new account");
+                Console.WriteLine("2. Deposit money");
+                Console.WriteLine("3. Withdraw money");
+                Console.WriteLine("4. Check balance");
+                Console.WriteLine("5. Exit");
+
+                //Set variables
+                choiceString = Console.ReadLine()?.Trim() ?? string.Empty;
+                if (int.TryParse(choiceString, out choiceInt) && choiceInt >= 1 && choiceInt <= 5)
+                {
+                    break;
+                }
+            } while (true);
+
+            //Menu Switch
+            switch (choiceInt)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Please enter your pin: ");
+                    if (!CheckPin(int.Parse(Console.ReadLine()?.Trim() ?? string.Empty)))
+                    {
+                        break;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("WARNING! This will delete your previous account.");
+                    Console.Write("Are you sure you want to continue: ");
+                    if ((Console.ReadLine()?.ToLower().Trim() ?? string.Empty) == "no")
+                    {
+                        break;
+                    }
+                    userAccount = CreateAccount();
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.Write("Please enter your pin: ");
+                    if (!CheckPin(int.Parse(Console.ReadLine()?.Trim() ?? string.Empty)))
+                    {
+                        break;
+                    }
+                    Console.Clear();
+                    Console.Write("How much would you like to deposit: £");
+                    userAccount.IncreaseBalance(double.Parse(Console.ReadLine()?.Trim() ?? string.Empty));
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.Write("Please enter your pin: ");
+                    if (!CheckPin(int.Parse(Console.ReadLine()?.Trim() ?? string.Empty)))
+                    {
+                        break;
+                    }
+                    Console.Clear();
+                    Console.Write("How much would you like to withdraw: £");
+                    userAccount.DecreaseBalance(double.Parse(Console.ReadLine()?.Trim() ?? string.Empty));
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.Write("Please enter your pin: ");
+                    if (!CheckPin(int.Parse(Console.ReadLine()?.Trim() ?? string.Empty)))
+                    {
+                        break;
+                    }
+                    Console.Clear();
+                    Console.WriteLine($"Name: {userAccount.GetName()}");
+                    Console.WriteLine($"Date of Birth: {userAccount.GetBirthDate()}");
+                    Console.WriteLine($"Bank Number: {userAccount.GetSortCode()}/{userAccount.GetAccountNumber()}");
+                    Console.WriteLine($"Balance: £{userAccount.GetBalance()}");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    return;
+                default:
+                    break;
+            }
+        } while (true);
     }
 
-} while (true);
-Banking Connor = new(864, new DateTime(2006, 09, 10), "Connor", 0146);
-Connor.DecreaseBalance(954.53);
-Console.WriteLine($"Name: {Connor.GetName()}");
-Console.WriteLine($"Date of Birth: {Connor.GetBirthDate()}");
-Console.WriteLine($"Bank Number: {Connor.GetSortCode()}/{Connor.GetAccountNumber()}");
-Console.WriteLine($"Balance: £{Connor.GetBalance()}");
+    private static bool CheckPin(int pin)
+    {
+        return pin == SwitchPin;
+    }
+
+    private static Banking CreateAccount()
+    {
+        //Declare variables
+        string firstName;
+        string lastName;
+        DateTime switchDOB;
+        double switchDeposit;
+
+        //Set variables
+        Console.Clear();
+        Console.WriteLine("Please enter the following information:");
+        Console.Write("First Name: ");
+        firstName = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
+        Console.Write("Last Name: ");
+        lastName = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
+        Console.Write("Date of Birth: ");
+        switchDOB = DateTime.Parse(Console.ReadLine()?.Trim() ?? string.Empty);
+        Console.Write("Deposit: ");
+        switchDeposit = double.Parse(Console.ReadLine()?.Trim() ?? string.Empty);
+        Console.Write("Pin: ");
+        SwitchPin = int.Parse(Console.ReadLine()?.Trim() ?? string.Empty);
+
+        //Return
+        return new(switchDeposit, switchDOB, char.ToUpper(firstName[0]) + firstName.Substring(1) + " " + char.ToUpper(lastName[0]) + lastName.Substring(1), SwitchPin);
+    }
+}
 
 class Banking
 {
@@ -81,7 +183,7 @@ class Banking
 
     public void DecreaseBalance(double amount)
     {
-        if (Balance - amount > 0)
+        if (Balance - amount >= 0)
         {
             Balance -= amount;
         }

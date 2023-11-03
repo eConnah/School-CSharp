7ecd67d6f30e78bd06dfef84397bd31bcd0a77aa
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-11-03 12:02:39 +0000

    Edit 1

--- RomanNumerals/RomanNumerals/Program.cs
+++ RomanNumerals/RomanNumerals/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        string userInput;
+
+        //Set variables
+        Console.Write("Please enter your roman numeral: ");
+        userInput = Console.ReadLine()?.Trim().ToUpper() ?? string.Empty;
+    }
+}

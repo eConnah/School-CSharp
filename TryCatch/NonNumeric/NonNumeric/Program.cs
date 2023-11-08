44eba63ee63b03120783869f7a1c02f39f5b213b
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-11-08 15:13:45 +0000

    new file:   TryCatch/NonNumeric/NonNumeric/Program.cs

---
+++ TryCatch/NonNumeric/NonNumeric/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        float numerator;
+        float denominator;
+
+        //Set variables
+        Console.Write("Please enter your numerator: ");
+        while (!float.TryParse(Console.ReadLine(), out numerator))
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+        Console.Write("Please enter your denominator: ");
+        while (!float.TryParse(Console.ReadLine(), out denominator))
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+
+        //Calculate
+        Console.WriteLine($"{numerator} / {denominator} = {numerator / denominator}");
+    }
+}

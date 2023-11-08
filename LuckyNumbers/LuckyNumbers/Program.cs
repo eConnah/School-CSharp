d3216acf9eb1ea7b9a672f0a6a352fc640d62d08
eConnah <63052937+eConnah@users.noreply.github.com>
2023-11-08 17:39:44 +0000

    new file:   LuckyNumbers/LuckyNumbers/Program.cs 	modified:   TryCatch/EmptyArray/EmptyArray/Program.cs

---
+++ LuckyNumbers/LuckyNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare and set variables
+        string userInput;
+        Console.Write("Please enter a number: ");
+        userInput = Console.ReadLine() ?? string.Empty;
+        bool unlucky = false;
+        List<char> unluckyNumbers = new();
+
+        //Loop
+        for (int i = 0; i < userInput.Length; i++)
+        {
+            for (int j = 0; j < userInput.Length; j++)
+            {
+                if (userInput[i] == userInput[j] && i != j)
+                {
+                    if (!unluckyNumbers.Contains(userInput[i]))
+                    {
+                        unluckyNumbers.Add(userInput[i]);
+                    }
+                    unlucky = true;
+                }
+            }
+        }
+
+        //Output
+        unluckyNumbers.Sort();
+        if (unlucky)
+        {
+            Console.Write("This number is unlucky. The repeated number(s) are: ");
+            foreach (char item in unluckyNumbers)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Your number is lucky!");
+        }
+    }
+}
--- TryCatch/EmptyArray/EmptyArray/Program.cs
+++ TryCatch/EmptyArray/EmptyArray/Program.cs
@@ -3,25 +3,27 @@
     private static void Main(string[] args)
     {
         //Declare variables and intilize
-        int[] intArray = Array.Empty<int>();
+        int[] intArray;
         string userInput;
-        int i = 0;
+        int arraySize;
 
         //Set variables
-        while (true)
+        Console.Write("How many numbers would you like to enter: ");
+        arraySize = Convert.ToInt32(Console.ReadLine());
+        intArray = new int[arraySize];
+        Console.WriteLine("Please enter your numbers.");
+        for (int i = 0; i < arraySize; i++)
         {
-            userInput = Console.ReadLine()?.Trim() ?? "rr";
-            if (userInput == "qq")
-            {
-                break;
-            }
-            if (userInput == "rr")
+            userInput = Console.ReadLine() ?? string.Empty;
+            if (userInput == string.Empty)
             {
                 Console.WriteLine("Empty inputs not accepted. Try again.");
-                return;
+                i--;
             }
-            intArray[i] = Convert.ToInt32(userInput);
-            i++;
+            else
+            {
+                intArray[i] = Convert.ToInt32(userInput);
+            }
         }
 
         //Output

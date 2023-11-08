883fb2391c511270e3a53ff199b9befd023aa9a3
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-11-08 15:30:36 +0000

    new file:   TryCatch/EmptyArray/EmptyArray/Program.cs 	new file:   TryCatch/Millennium/Millennium/Program.cs

---
+++ TryCatch/EmptyArray/EmptyArray/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables and intilize
+        int[] intArray = Array.Empty<int>();
+        string userInput;
+        int i = 0;
+
+        //Set variables
+        while (true)
+        {
+            userInput = Console.ReadLine()?.Trim() ?? "rr";
+            if (userInput == "qq")
+            {
+                break;
+            }
+            if (userInput == "rr")
+            {
+                Console.WriteLine("Empty inputs not accepted. Try again.");
+                return;
+            }
+            intArray[i] = Convert.ToInt32(userInput);
+            i++;
+        }
+
+        //Output
+        Console.WriteLine(intArray.Average());
+    }
+}
---
+++ TryCatch/Millennium/Millennium/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int numerator;
+        int denominator;
+        bool negetiveException = false;
+
+        //Set variables
+        do
+        {
+            Console.Write("Please enter your numerator: ");
+            while (!int.TryParse(Console.ReadLine(), out numerator))
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+            try
+            {
+                if (numerator < 0 || numerator > 1000)
+                {
+                    throw new IndexOutOfRangeException("Your number cannot be below 0 or above 1000.");
+                }
+                else
+                {
+                    negetiveException = false;
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                negetiveException = true;
+                Console.WriteLine(ex.Message);
+            }
+        } while (negetiveException);
+        do
+        {
+            Console.Write("Please enter your denominator: ");
+            while (!int.TryParse(Console.ReadLine(), out denominator))
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+            try
+            {
+                if (denominator < 0 || denominator > 1000)
+                {
+                    throw new IndexOutOfRangeException("Your number cannot be below 0 or above 1000.");
+                }
+                else
+                {
+                    negetiveException = false;
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                negetiveException = true;
+                Console.WriteLine(ex.Message);
+            }
+        } while (negetiveException);
+
+        //Calculate
+        Console.WriteLine($"{numerator} / {denominator} = {numerator / denominator}");
+    }
+}

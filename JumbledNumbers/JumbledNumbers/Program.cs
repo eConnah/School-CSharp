c38bb1fa348d04efd528794cb76a2db91fc23fc6
eConnah <63052937+eConnah@users.noreply.github.com>
2023-11-08 18:14:23 +0000

    Created JumbledNumbers

---
+++ JumbledNumbers/JumbledNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        string userInput;
+        int[] intArray;
+
+        //Set variables
+        Console.Write("Please enter your number: ");
+        userInput = Console.ReadLine() ?? string.Empty;
+        intArray = new int[userInput.Length];
+
+        //Convert
+        for (int i = 0; i < userInput.Length; i++)
+        {
+            intArray[i] = int.Parse(userInput[i].ToString());
+        }
+
+        //Check
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            if (i + 1 < intArray.Length)
+            {
+                if (intArray[i] - intArray[i + 1] > 1 || intArray[i] - intArray[i + 1] < -1)
+                {
+                    Console.WriteLine("Your number is not jumbled.");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("Your number is jumbled.");
+    }
+}

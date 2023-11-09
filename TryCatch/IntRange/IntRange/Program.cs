2c1f43103ca04208ad8c1f985a7acd88a42b4f0e
eConnah <63052937+eConnah@users.noreply.github.com>
2023-11-09 10:59:23 +0000

    Created IntRange and StringConversion

---
+++ TryCatch/IntRange/IntRange/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare and initilise variables
+        int i = 0;
+        string userInput;
+        int userNumber = 0;
+        List<int> userInputs = new();
+        bool escape = false;
+
+        //Set variables
+        Console.WriteLine("Enter your numbers, and type qq when you are done.");
+        while (!escape)
+        {
+            i++;
+            do
+            {
+                Console.Write($"Please enter number {i}: ");
+                userInput = Console.ReadLine()?.Trim() ?? string.Empty;
+                if (userInput == "qq")
+                {
+                    escape = true;
+                    break;
+                }
+            } while (!int.TryParse(userInput, out userNumber));
+            userInputs.Add(userNumber);
+        }
+    }
+}
---
+++ TryCatch/StringConversion/StringConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables 
+        int interger;
+
+        //Set variables
+        Console.Write("Please enter a number: ");
+        while (!int.TryParse(Console.ReadLine(), out interger))
+        {
+            Console.WriteLine("Please enter a valid interger.");
+        }
+
+        //Output
+        Console.WriteLine("You entered: " + interger);
+    }
+}

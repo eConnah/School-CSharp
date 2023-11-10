b22fcef5b93440f86002fde9a418fa809932cfd3
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-11-10 09:36:58 +0000

    Created SweetBags

---
+++ SweetBags/SweetBags/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int sweetBags;
+        int sweets;
+
+        //Set variables
+        Console.Write("How many sweet bags do you have: ");
+        sweetBags = int.Parse(Console.ReadLine() ?? string.Empty);
+        Console.Write("How many sweets do you have: ");
+        sweets = int.Parse(Console.ReadLine() ?? string.Empty);
+
+        //Convert
+        if ((sweets - (sweetBags - 1)) % 2 != 0)
+        {
+            Console.WriteLine("You can have an odd number of sweets in each bag.");
+        }
+        else
+        {
+            Console.WriteLine("You can't have an odd number of sweets in each bag.");
+        }
+    }
+}

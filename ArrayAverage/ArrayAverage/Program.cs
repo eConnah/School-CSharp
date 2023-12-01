295c4cf9c95eceeacf429d23bf94188197bd4032
Connor <93721060+18AlecksC@users.noreply.github.com>
2023-12-01 12:24:50 +0000

    Arrays

---
+++ ArrayAverage/ArrayAverage/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int arrayLength;
+        int[] inputArray = new int[5];
+
+        //Set variables
+        do
+        {
+            arrayLength = InputValidator("Please enter the length of the array you would like (must be above 0): ");
+        } while (arrayLength < 1);
+        for (int i = 0; i < arrayLength; i++)
+        {
+            inputArray[i] = InputValidator($"Enter number {i + 1}: ");
+        }
+
+        //Print
+        Console.Clear();
+        Console.WriteLine($"The sum of the array is: {inputArray.Sum()}.");
+        Console.WriteLine($"The average of the array is: {inputArray.Average()}.");
+    }
+
+    private static int InputValidator(string prompt)
+    {
+        //Set variables
+        Console.Clear();
+        do
+        {
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                return number;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("An invalid input has been entered.");
+            }
+        } while (true);
+    }
+}
---
+++ ArraySorting/ArraySorting/Program.cs
@@ -0,0 +1,77 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int arrayLength;
+        int[] inputArray;
+
+        //Set variables
+        do
+        {
+            arrayLength = InputValidator("Please enter the length of the array you would like (must be above 0): ");
+        } while (arrayLength < 1);
+        inputArray = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            inputArray[i] = InputValidator($"Enter number {i + 1}: ");
+        }
+
+        //Print
+        Console.Clear();
+        Console.Write($"The unsorted array is: ");
+        foreach (int item in inputArray)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+        Console.Write($"The sorted array is: ");
+        foreach (int item in BubbleSort(inputArray))
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+
+    private static int InputValidator(string prompt)
+    {
+        //Set variables
+        Console.Clear();
+        do
+        {
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                return number;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("An invalid input has been entered.");
+            }
+        } while (true);
+    }
+
+    private static int[] BubbleSort(int[] inputArray)
+    {
+        //Declare and initilise variables
+        bool passed = false;
+
+        //Sort
+        do
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if (inputArray[i] > inputArray[i + 1])
+                {
+                    (inputArray[i], inputArray[i + 1]) = (inputArray[i + 1], inputArray[i]);
+                    i++;
+                    passed = true;
+                }
+            }
+        } while (passed);
+
+        //Return
+        return inputArray;
+    }
+}

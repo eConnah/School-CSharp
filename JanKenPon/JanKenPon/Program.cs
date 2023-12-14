f7187796e9cbeb63cc3a5a460c20499cb21cb1cc
eConnah <63052937+eConnah@users.noreply.github.com>
2023-12-14 09:26:55 +0000

    RockPaperScissors

---
+++ JanKenPon/JanKenPon/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+
+    private static string RockPaperScissors(int hand)
+    {
+        return hand switch
+        {
+            1 => "Rock",
+            2 => "Paper",
+            3 => "Scissors",
+            _ => string.Empty,
+        };
+    }
+}

76342229ec29d2bc792143d62bfabad9ac5b4177
18alecksc <18alecksc@gmail.com>
2024-01-12 12:27:15 +0000

    update

---
+++ TwoWords/TwoWords/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        string inputOne;
+        string inputTwo;
+        
+
+        //Set variables
+        inputOne = AcceptInput("Please enter your first word: ");
+        inputTwo = AcceptInput("Please enter your second word: ");
+
+        inputTwo.ToList
+    }
+
+    private static string AcceptInput(string prompt)
+    {
+        //Declare variables
+        string userInput;
+
+        //Accept input
+        Console.Clear();
+        while (true)
+        {
+            Console.Write(prompt);
+            userInput = Console.ReadLine()?.Trim() ?? string.Empty;
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                return userInput;
+            }
+            Console.Clear();
+            Console.WriteLine("Please enter a proper string.");
+        }
+    }
+}

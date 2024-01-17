a95808cbb8b987eb2c3f88a94d0687f121d9bfea
eConnah <63052937+eConnah@users.noreply.github.com>
2024-01-17 11:27:58 +0000

    Started NibbleToHex

---
+++ NibbleToHex/NibbleToHex/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare and intialise variables
+        string binaryInput;
+        List<char> output = new();
+
+        //Set variables
+        Console.Write("Please enter your binary input: ");
+        binaryInput = Console.ReadLine()?.Trim() ?? string.Empty;
+        for (int i = 0; i < binaryInput.Length; i += 4)
+        {
+            int substringLength = Math.Min(4, binaryInput.Length - i);
+            output.Add(ToHex(binaryInput.Substring(i, substringLength)));
+        }
+    }
+
+    private static char ToHex(string nibble)
+    {
+
+        return;
+    }
+}

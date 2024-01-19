0c34186988246536982c0746d6ceba505851c41a
18alecksc <18alecksc@gmail.com>
2024-01-19 10:38:15 +0000

    Completed BracketChecker

---
+++ BracketChecker/BracketChecker/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare and initilise variables
+        int bracketCount = 0;
+        string userInput;
+        bool isInvalid = false;
+
+        //Get user input
+        Console.Clear();
+        Console.Write("Please enter your string with brackets: ");
+        userInput = Console.ReadLine()?.Trim() ?? string.Empty;
+        Console.Clear();
+
+        //Check for brackets
+        foreach (char item in userInput)
+        {
+            if (item == '(')
+            {
+                bracketCount++;
+            }
+            else if (item == ')')
+            {
+                bracketCount--;
+            }
+            if (bracketCount < 0)
+            {
+                isInvalid = true;
+                break;
+            }
+        }
+
+        //Output
+        if (isInvalid)
+        {
+            Console.WriteLine("This string with brackets is invalid.");
+        }
+        else
+        {
+            Console.WriteLine("This string with brackets is valid.");
+        }
+    }
+}

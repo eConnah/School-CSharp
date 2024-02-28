a784f134549fd64f55a337cfb18d9c86165e9cf7
eConnah <63052937+eConnah@users.noreply.github.com>
2024-02-28 11:12:51 +0000

    Created Countdown

---
+++ Countdown/Countdown/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        int countdown;
+
+        //Set variables
+        countdown = GrabNumber("What would you like to countdown from: ");
+
+        //Countdown
+        for (int i = countdown; i > 0; i--)
+        {
+            Console.WriteLine(i);
+            Task.Delay(1000).Wait();
+            Console.Clear();
+        }
+        Console.WriteLine("LIFT OFF!");
+    }
+
+    private static int GrabNumber(string prompt)
+    {
+        //Declare variables
+        string userInput;
+        int number;
+
+        //Set variables
+        Console.Clear();
+        while (true)
+        {
+            Console.Write(prompt);
+            userInput = Console.ReadLine()?.Trim() ?? string.Empty;
+            if (int.TryParse(userInput, out number))
+            {
+                Console.Clear();
+                return number;
+            }
+            Console.Clear();
+            Console.WriteLine("Invalid input please try again.");
+        }
+    }
+}

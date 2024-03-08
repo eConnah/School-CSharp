836954376333402bd5eb64b474f2932ff60c3971
18alecksc <18alecksc@gmail.com>
2024-03-08 10:00:06 +0000

    Started StaticQueue

---
+++ StaticQueue/StaticQueue/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+}
+
+public class Queue<T>
+{
+    private T[] staticArray;
+    private int maxSize;
+    private int size;
+    private int frontPointer;
+    private int rearPointer;
+    Queue()
+    {
+
+    }
+}

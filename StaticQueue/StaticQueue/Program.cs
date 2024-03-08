ffa1b6285b85f223bd3a8d61560d16c0854176af
18alecksc <18alecksc@gmail.com>
2024-03-08 12:26:24 +0000

    Finished StaticQueue

--- StaticQueue/StaticQueue/Program.cs
+++ StaticQueue/StaticQueue/Program.cs
@@ -2,6 +2,12 @@
 {
     private static void Main(string[] args)
     {
+        Queue<int> queueOne = new(5);
+        queueOne.EnQueue(15);
+        queueOne.EnQueue(1);
+        queueOne.EnQueue(9);
+        queueOne.EnQueue(6);
+        Console.WriteLine(queueOne.Peek());
     }
 }
 
@@ -9,11 +15,66 @@ public class Queue<T>
 {
     private T[] staticArray;
     private int maxSize;
-    private int size;
+    private int currentSize;
     private int frontPointer;
     private int rearPointer;
-    Queue()
+    public Queue(int maxSize)
     {
+        this.maxSize = maxSize;
+        staticArray = new T[maxSize];
+        frontPointer = 0;
+        rearPointer = -1;
+    }
 
+    public bool IsFull()
+    {
+        return rearPointer == maxSize - 1;
+    }
+
+    public bool IsEmpty()
+    {
+        return currentSize == 0;
+    }
+
+    public void EnQueue(T item)
+    {
+        if (IsFull())
+        {
+            throw new InvalidOperationException("Queue is full");
+        }
+        else
+        {
+            rearPointer++;
+            currentSize++;
+            staticArray[rearPointer] = item;
+        }
+    }
+
+    public T DeQueue()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        else
+        {
+            T item = staticArray[frontPointer];
+            staticArray[frontPointer] = default;
+            frontPointer++;
+            currentSize--;
+            return item;
+        }
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        else
+        {
+            return staticArray[frontPointer];
+        }
     }
 }

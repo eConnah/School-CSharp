701a573b54b2aea03e0073e51a65c9a292edbf92
eConnah <63052937+eConnah@users.noreply.github.com>
2024-03-28 11:46:15 +0000

    Fixed PigLatin

--- PigLatin/PigLatin/Program.cs
+++ PigLatin/PigLatin/Program.cs
@@ -3,15 +3,23 @@
     private static void Main(string[] args)
     {
         //Declare variables
-        char temp;
+        char firstChar;
         string word;
+        List<char> vowels = new() { 'a', 'e', 'i', 'o', 'u' };
 
         //Set variables
         Console.Write("Please enter your word: ");
-        word = Console.ReadLine()?.Trim() ?? string.Empty;
-        temp = word[0];
-        word = word[1..];
-        word = word + '-' + temp + "ay";
+        word = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
+        firstChar = word[0];
+        if (vowels.Contains(firstChar))
+        {
+            word += "-yay";
+        }
+        else
+        {
+            word = word[1..];
+            word = word + '-' + firstChar + "ay";
+        }
 
         //Output
         Console.WriteLine($"The pig latin of your word is {word}.");

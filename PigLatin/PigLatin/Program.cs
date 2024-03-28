1d12f5b93eac8083fdad08e1921933fc95c66b71
eConnah <63052937+eConnah@users.noreply.github.com>
2024-03-28 11:36:48 +0000

    Completed PigLatin

---
+++ PigLatin/PigLatin/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare variables
+        char temp;
+        string word;
+
+        //Set variables
+        Console.Write("Please enter your word: ");
+        word = Console.ReadLine()?.Trim() ?? string.Empty;
+        temp = word[0];
+        word = word[1..];
+        word = word + '-' + temp + "ay";
+
+        //Output
+        Console.WriteLine($"The pig latin of your word is {word}.");
+    }
+}

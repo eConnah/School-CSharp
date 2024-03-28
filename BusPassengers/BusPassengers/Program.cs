931312d667b550d45cbf3926563d43763916df0c
eConnah <63052937+eConnah@users.noreply.github.com>
2024-03-28 11:29:15 +0000

    COmpleted BusPassengers

---
+++ BusPassengers/BusPassengers/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Declare and initialise variables
+        int[] leavingP;
+        int[] joiningP;
+        int passengers = 0;
+        int maxPassengers = 0;
+
+        //Set variables
+        leavingP = GetInts("Enter number of passengers leaving: ");
+        joiningP = GetInts("Enter number of passengers joining: ");
+
+        //Calculate number of passengers
+        for (int i = 0; i < leavingP.Length; i++)
+        {
+            passengers -= leavingP[i];
+            passengers += joiningP[i];
+            if (passengers > maxPassengers)
+            {
+                maxPassengers = passengers;   
+            }
+        }
+
+        //Output
+        Console.WriteLine($"Maximum number of passengers at once: {maxPassengers}");
+    }
+
+    private static int[] GetInts(string prompt)
+    {
+        Console.Clear();
+        while (true)
+        {
+            Console.Write(prompt);
+            return Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        }
+    }
+}

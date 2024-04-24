6f136818b0ecb78ec540d2127e02be179548196c
18alecksc <18alecksc@gmail.com>
2024-04-24 15:29:43 +0100

    Modified Binary Tree

--- Binary Tree/Binary Tree/Program.cs
+++ Binary Tree/Binary Tree/Program.cs
@@ -3,119 +3,16 @@ using System.Security.AccessControl;
 
 internal class Program
 {
-    private static int treeLength = 0;
     private static void Main(string[] args)
     {
         //Declare and initilise Variables
-        string treeInput;
-        string childrenInput;
         string[] binaryTree;
         string[] children;
-        List<int> childrenElements = new();
-        int row = 0;
-        int column = 0;
-        bool isRoot = false;
 
         //Set variables
         Console.Write("Please enter your binary tree: ");
-        treeInput = Console.ReadLine()?.Trim() ?? string.Empty;
+        binaryTree = Console.ReadLine()?.Trim().Split(' ') ?? Array.Empty<string>();
         Console.Write("Please enter your children: ");
-        childrenInput = Console.ReadLine()?.Trim() ?? string.Empty;
-        binaryTree = treeInput.Split(' ');
-        children = childrenInput.Split(' ');
-        treeLength = binaryTree.Length;
-
-        //Add Children Index
-        foreach (string item in children)
-        {
-            childrenElements.Add(Array.IndexOf(binaryTree, item));
-        }
-
-        //Find Root
-        while (!isRoot)
-        {
-            switch (InTree(row, column, childrenElements[0], childrenElements[1]))
-            {
-                case 0:
-                    column++;
-                    break;
-                case 1:
-                    isRoot = true;
-                    break;
-                case 2:
-                    row++;
-                    break;
-            }
-        }
-
-        //Print Root
-        Console.WriteLine($"The ansestor of {childrenInput} is {binaryTree[ToElement(row, column)]}");
-
-    }
-
-    private static int ToElement(int row, int column)
-    {
-        int element = 0;
-
-        for (int i = 0; i < row; i++)
-        {
-            element *= 2;
-            element++;
-        }
-        element += column;
-        return element;
-    }
-
-    private static (int row, int column) FromElement(int element)
-    {
-        int i = 0;
-        while (true)
-        {
-            if (element - ToElement(i, 0) >= 0)
-            {
-                i++;
-            }
-            else
-            {
-                i--;
-                break;
-            }
-        }
-        return (i, element - ToElement(i, 0));
-    }
-
-    private static int InTree(int rRow, int rColumn, int childOne, int childTwo)
-    {
-        //Declare and initilise variables
-        int row = rRow + 1;
-        int column = 0;
-        int element = ToElement(row, column);
-        int i = 0;
-
-        //Loop
-        while (element < treeLength)
-        {
-            if (element == childOne)
-            {
-                i++;
-            }
-            if (element == childTwo)
-            {
-                i++;
-            }
-            if (column < 0.5 * Math.Pow(2, row))
-            {
-                element++;
-                column++;
-            }
-            else
-            {
-                column = 0;
-                row++;
-            }
-        }
-
-        //Return
-        return i;
+        children = Console.ReadLine()?.Trim().Split(' ') ?? Array.Empty<string>();
     }
 }

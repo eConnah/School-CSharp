9d45f9e5bcb1c29abcd0fa8f07ce796dabec49e0
18alecksc <18alecksc@gmail.com>
2024-05-01 15:28:21 +0100

    Completed EvenSum

---
+++ EvenSum/EvenSum/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine(SumEven(10));
+    }
+
+    public static int SumEven(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n % 2 == 0)
+        {
+            return n + SumEven(n - 2);
+        }
+        else
+        {
+            return n + SumEven(n - 1);
+        }
+    }
+}

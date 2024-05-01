7af971f220af0154d8aea0e43d6093500d3cf829
eConnah <63052937+eConnah@users.noreply.github.com>
2024-05-01 13:26:31 +0100

    heavily progressed BinaryTree

--- BinaryTree/BinaryTree/Program.cs
+++ BinaryTree/BinaryTree/Program.cs
@@ -8,7 +8,8 @@ class Program
         //Declare and initilise Variables
         string[] inputTree;
         string[] children;
-        BNode tree = new(' ');
+        BNode tree;
+        List<int> path = new();
 
         //Set variables
         Console.Write("Please enter your binary tree: ");
@@ -16,33 +17,93 @@ class Program
         Console.Write("Please enter your children: ");
         children = Console.ReadLine()?.Trim().Split(' ') ?? Array.Empty<string>();
 
-        for (int i = 0; i < inputTree.Length; i++)
+        //Fill Tree
+        tree = new BNode(Convert.ToChar(inputTree[0]));
+        for (int i = 1; i < inputTree.Length; i++)
         {
+            path = PathGenerator(i);
             if (inputTree[i] != "None")
             {
-                tree.left.data = Convert.ToChar(inputTree[i]);
-            }
-        }
-
-    }
-
-    public static (BNode, bool) FillTree(BNode tree, char input)
-    {
-        if (tree.left.data != null)
-        {
-            if (tree.right.data != null)
-            {
-                FillTree(tree.left, input);
+                FillTree(tree, Convert.ToChar(inputTree[i]), path, 0);
             }
             else
             {
-                tree.right.data = input;
+                FillTree(tree, ' ', path, 0);
             }
         }
-        else
+
+        Console.WriteLine();
+    }
+
+    public static void FillTree(BNode tree, char input, List<int> path, int index)
+    {
+        for (int i = index; i < path.Count; i++)
         {
-            tree.left.data = input;
+            if (path[i] == 1)
+            {
+                if (tree.left == null)
+                {
+                    tree.left = new BNode(input);
+                }
+                else
+                {
+                    FillTree(tree.left, input, path, i + 1);
+                }
+            }
+            else if (path[i] == 2)
+            {
+                if (tree.right == null)
+                {
+                    tree.right = new BNode(input);
+                }
+                else
+                {
+                    FillTree(tree.right, input, path, i + 1);
+                }
+            }
         }
+    }
+
+    public static List<int> PathGenerator(int index)
+    {
+        //Variables
+        List<int> path = new();
+        int ones = 0;
+        int twoPower = 10;
+        double difference;
+        double powerOne = 0;
+        double powerTwo;
+
+        //Path Generation
+        while (index >= powerOne)
+        {
+            ones++;
+            powerOne = Math.Pow(2, ones) - 1;
+        }
+        ones--;
+        for (int i = 0; i < ones; i++)
+        {
+            path.Add(1);
+        }
+        difference = index - powerOne;
+        while (twoPower > 0)
+        {
+            powerTwo = Math.Pow(2, twoPower);
+            if (difference < powerTwo)
+            {
+                twoPower--;
+            }
+            else
+            {
+                path[^(twoPower + 1)] = 2;
+                difference -= powerTwo;
+            }
+        }
+        if (index % 2 == 0)
+        {
+            path[^1] = 2;
+        }
+        return path;
     }
 
     public static void RecursiveInorder(BNode root)

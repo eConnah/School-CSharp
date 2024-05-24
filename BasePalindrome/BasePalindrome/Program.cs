361dafe5bf22d5c25c7337a1b6a59e7d32753801
eConnah <alecks.connor@gmail.com>
2024-05-24 15:59:05 +0100

    Completed (BasePalindrome)

--- BasePalindrome/BasePalindrome/Program.cs
+++ BasePalindrome/BasePalindrome/Program.cs
@@ -3,38 +3,70 @@
     private static void Main(string[] args)
     {
         int currentBaseTen = 0;
-        List<int> baseTen = new();
-        List<int> baseTwo = new();
-        List<(int, int)> palindromes = new();
-    }
-
-    private static List<int> BinaryConverter(int number)
-    {
-        List<int> baseTwo = new();
-        int halvedInt = number;
-        while (halvedInt > 1)
+        string baseTen;
+        string baseTwo;
+        (string, string)? result;
+        List<(string, string)> palindromes = new();
+        while (palindromes.Count <= 20)
         {
-            baseTwo.Add(number % 2);
-            halvedInt = number / 2;
-        }
-        baseTwo.Reverse();
-        return baseTwo;
-    }
-
-    private static List<(int, int)> IsPalindrome(List<int> originalTen, List<int> originalTwo)
-    {
-        List<int> reversedTen = new();
-        reversedTen = originalTen;
-        reversedTen.Reverse();
-        List<int> reversedTwo = new();
-        reversedTwo = originalTwo;
-        reversedTwo.Reverse();
-        if (originalTen == reversedTen)
-        {
-            if (reversedTwo == originalTwo)
+            baseTen = currentBaseTen.ToString();
+            baseTwo = BinaryConverter(currentBaseTen);
+            result = IsPalindrome(baseTen, baseTwo);
+            if (result != null)
             {
-                return (int.Parse(originalTen.ToString()), int.Parse(originalTwo.ToString()));
+                palindromes.Add(result.Value);
+            }
+            currentBaseTen++;
+        }
+        foreach ((string, string) item in palindromes)
+        {
+            Console.WriteLine(item);   
+        }
+    }
+
+    private static string BinaryConverter(int number)
+    {
+        if (number == 0)
+        {
+            return "0";   
+        }
+        string baseTwo = string.Empty;
+        int halvedInt = number;
+        while (halvedInt > 0)
+        {
+            baseTwo += halvedInt % 2;
+            halvedInt /= 2;
+        }
+        return string.Concat(baseTwo.Reverse());
+    }
+
+    private static (string, string)? IsPalindrome(string originalTen, string originalTwo)
+    {
+        string reversedTen;
+        string reversedTwo;
+        if (originalTen.Length != 1)
+        {
+            reversedTen = string.Concat(originalTen.Reverse());
+        }
+        else
+        {
+            reversedTen = originalTen;
+        }
+        if (originalTwo.Length != 1)
+        {
+            reversedTwo = string.Concat(originalTwo.Reverse());
+        }
+        else
+        {
+            reversedTwo = originalTwo;
+        }
+        if (string.Compare(originalTen, reversedTen) == 0)
+        {
+            if (string.Compare(reversedTwo, originalTwo) == 0)
+            {
+                return (originalTen, originalTwo);
             }
         }
+        return null;
     }
 }

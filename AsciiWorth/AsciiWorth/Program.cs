d7a79e82fa179b7fa2a36b297e0f44a5bec8075f
18alecksc <18alecksc@gmail.com>
2024-06-07 10:36:48 +0100

    Completed Projects (AsciiWorth && FaceCounter)

---
+++ AsciiWorth/AsciiWorth/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Please enter your word: ");
+        string word = Console.ReadLine();
+        int worth = 0;
+        foreach (char item in word)
+        {
+            worth += Convert.ToInt32(item);
+        }
+        Console.WriteLine($"The value of your word is {worth}.");
+    }
+}
---
+++ FaceCounter/FaceCounter/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int happinessNumber = 0;
+        string userInput;
+        Console.Write("Please enter your string: ");
+        userInput = Console.ReadLine();
+        for (int i = 1; i < userInput.Length; i++)
+        {
+            if (userInput[i] == ':')
+            {
+                if (userInput[i - 1] == ')')
+                {
+                    happinessNumber--;
+                }
+                else if (userInput[i - 1] == '(')
+                {
+                    happinessNumber++;
+                }
+            }
+            else if (userInput[i] == ')')
+            {
+                if (userInput[i - 1] == ':')
+                {
+                    happinessNumber++;
+                }
+            }
+            else
+            {
+                if (userInput[i - 1] == ':')
+                {
+                    happinessNumber--;
+                }
+            }
+        }
+        Console.WriteLine($"Your happiness number is: {happinessNumber}");
+    }
+}
